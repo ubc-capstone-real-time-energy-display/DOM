31457eb0e99f695c091a2e5c2531441ad76b3463
Alvin Lao <alvin.lao.is@gmail.com>
2015-11-22 15:20:14 -0800

    Fix curl and loading HTML into DOMDocument

--- index.php
+++ index.php
@@ -2,16 +2,21 @@
 <html>
 <body>
 <?php 
-	$curl_scraped_page = new DOMDocument();
 	//open the html
-	$url = "file:///C:/xampp/htdocs/test%20text.html";
+	$url = "localhost/DOM/test%20text.html";
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	$curl_scraped_page = curl_exec($ch);
+	$content = curl_exec($ch);
 	curl_close($ch);
+
+    // Print the content
+    //echo htmlspecialchars($content);
+
+    $dom = new DOMDocument();
+    $dom->loadHTML($content);
 	
 	$links = array();
-	$container1 = $curl_scraped_page->getElementById("table-data");//find all element with id table-data
+	$container1 = $dom->getElementById("table-data");//find all element with id table-data
 	$container2 = $container1->getElementsByTagName("tr");//find all element inside tag <tr>
 	$data = $container2->getElementByTagName("td");//find all element inside tag <td>
 		//print out the data
