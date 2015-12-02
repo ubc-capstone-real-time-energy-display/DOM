62627c5a85e7fbf52c5c874c88554fddd89f4bb1
TuanPham303 <phamcongtuan303@gmail.com>
2015-12-02 13:23:08 -0800

    header file is empty (maybe)

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<!DOCTYPE html>
-<html>
-<body>
+
 <?php 
-	//open the html
+    //open the html
     $url = "http://ion.energy.ubc.ca/ion/Historical/DisplayLogs.aspx?queryId=831317ed-1695-4a28-a197-9d9bad7e727e&dgm=x-pml:/diagrams/ud/UBC_SUS/sub_diagrams/sub_diagram_totem%20park.dgm&node=VIP.BIS-APPIONPME-P&logServerName=QUERYSERVER.BIS-APPIONPME-P&logServerHandle=327952&isEventLog=";
 
     $ch = curl_init($url);
@@ -19,103 +17,104 @@
 
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt ($ch, CURLOPT_HTTPGET, TRUE); 
     $content=curl_exec($ch);
 
     /*
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt ($ch, CURLOPT_HTTPGET, TRUE); 
-	$content = curl_exec($ch);
+    $content = curl_exec($ch);
      */
-	curl_close($ch);
+    curl_close($ch);
 
     // Print the content
-    //echo htmlspecialchars($content);
+    //ec4ho htmlspecialchars($content);
     //echo $content;
+
 
     $dom = new DOMDocument();
     $dom->loadHTML($content);
 
-    $header = array();
-	$header1 = $dom->getElementById("hdr"); //find all elements with id hdr
-	$header2 = $header1->getElementsByTagName("th"); //find all elements inside tag <th>
-	foreach ($header2 as $building) {
-		$arr1 = $bulding->getElementsByTagName("input"); //find all the building name inside tag <input>
-		$header[] = array(
-			'name' => $arr1
-			);
-	}
-	//$timeheader = $header[0];
-	//echo $timeheader;  //frint out the time header
-	foreach ($header as $value) {
-		$buildings = $value['name'];
-		for ($a = 0; $a < $buildings->length; $a++){
-			echo $buildings->$building($a)->nodeValue . "<br />"; //print out the building header
-		}
-
-	}
-
-	
-	$data = array();
-	$container1 = $dom->getElementById("data");//find all element with id data 
-	$container2 = $container1->getElementsByTagName("tr");//find all element inside tag <tr>
-	foreach($container2 as $item) {
-		$arr = $item->getElementsByTagName("td");//find all element inside tag <td>
-		$data[] = array(
-			'number' => $arr
-		);
-	}
-	//$timedata = $data[0];
-	//echo $timedata;  //print out time data
-	foreach($data as $element) {
-        $items = $element['number'];
-        for ($i = 0; $i < $items->length; $i++) {
-            echo $items->item($i)->nodeValue . "<br />"; 
+    $data1 = array();
+    $header1 = $dom->getElementById("hdr");
+    foreach($header1 as $input) {
+        //$inputs = $data1['name'];
+        for ($a = 0; $a < $header1->length; $a++){
+            echo $header1->input($a)->nodeValue . "<br />";
         }
     }
-	
-	
-	
-	/*
-	formating 
-	<table>
-		<tr>
-			<td>$timeheader</td>
-			<td>buildingheader(1)</td>
-			<td>buildingheader(2)</td>
-			<td>buildingheader(3)</td>
-			<td>buildingheader(4)</td>
-			<td>buildingheader(5)</td>
-			<td>buildingheader(6)</td>
-			<td>buildingheader(7)</td>
-			<td>buildingheader(8)</td>
-		</tr>
-		<tr>
-			<td>$timedata<td>
-			<td>item(1)</td>
-			<td>item(2)</td>
-			<td>item(3)</td>
-			<td>item(4)</td>
-			<td>item(5)</td>
-			<td>item(6)</td>
-			<td>item(7)</td>
-			<td>item(8)</td>
-		</tr>
-	</table>
-	*/
-	
-	
-	
-	
-	
-	
-	//echo $curl_scraped_page;
-	//$data = file_get_contents('test text.html');
-	//$regex = '/I am (.+?) this morning/';
-	//preg_match_all($regex,$curl_scraped_page,$match);
-	//var_dump($match); 
-	//echo $match[1];
-	//for ()
-	?>
-</body>
-</html>
+    /*$header2 = $header1->getElementsByTagName("th");
+    foreach ($header2 as $value){
+        $arr1 = $value->getElementsByTagName("input");
+        $data1[] = array(
+            'name' => $arr1
+            );
+    }
+
+    foreach($data1 as $input) {
+        $inputs = $data1['name'];
+        for ($a = 0; $a < $inputs->length; $a++){
+            echo $inputs->input($a)->nodeValue . "<br />";
+        }
+    }
+        
+    
+
+    //$header3 = $header2->getElementById("hdr");
+    //$header3 = $header2->getElementsByTagName("span");
+    //$header4 = $header3->getElementsByTagName("th"); //find all elements inside tag <th>
+    /*foreach ($header2 as $building) {
+        $arr1 = $building->getElementsByTagName("input"); //find all the building name inside tag <input>
+        $data[] = array(
+            'name' => $arr1
+            );
+    }
+    //$timeheader = $header[0];
+    //echo $timeheader;  //frint out the time header
+    foreach ($data as $value) {
+        $buildings = $value['name'];
+        for ($a = 0; $a < $buildings->length; $a++){
+            echo $buildings->$building($a)->nodeValue . "<br />"; //print out the building header
+        }
+
+    }*/
+    
+    
+    $data = array();
+    $container1 = $dom->getElementById("data");//find all element with id data 
+    $container2 = $container1->getElementsByTagName("tr");//find all element inside tag <tr>
+    foreach($container2 as $item) {
+        $arr = $item->getElementsByTagName("td");//find all element inside tag <td>
+        $data[] = array(
+            'number' => $arr
+        );
+    }
+    
+   /* foreach($data as $element) {
+        $items = $element['number'];
+        for ($i = 0; $i < $items->length; $i++) {
+
+            echo $items->item($i)->nodeValue . "<br />"; 
+        }
+    }*/
+    function build_table($data){
+        $html = '<table>';
+        foreach($data as $energy){
+            
+        $html .= '<tr>';
+        $items = $energy['number'];
+            for ($i = 0; $i < $items->length; $i++){
+            $html .= '<td>' . $items->item($i)->nodeValue . '</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    // finish table and return it
+
+    $html .= '</table>';
+    return $html;
+    }
+    echo build_table($data);
+?>
+
